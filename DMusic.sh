86bc65203ca63f6b1c7d5bb3fd4c6259f9d18993
zereft01 <80718011+zereft01@users.noreply.github.com>
2021-07-21 15:07:01 -0600

    Update DMusic.sh

--- DMusic.sh
+++ DMusic.sh
@@ -3,7 +3,7 @@
 lineas="===================================="                                   
 green='\033[1;32m'
 printf $green
-v=1.0
+v=1.2
 echo $lineas
 
 echo "  ╔═ ╔╔ ║ ║╔═╝╝╔═╝  ═║  ╔═║
@@ -16,13 +16,13 @@ echo $lineas
 read -p "¿Que quieres descargar?
  [1] Canción 
  [2] Playlist 
- [3] Salir " arch
+ [3] Salir __>" arch
 
 
 case $arch in
 	[Cc]ancion|Canción|canción|1)
 		echo $lineas
-		read -p "Pega la URL de la canción " Ind
+		read -p "Pega la URL de la canción __>" Ind
 		if [ -z $Ind ];then
 			echo $lineas
 			echo "No ingresante una url"
@@ -31,21 +31,21 @@ case $arch in
 		
 		sleep 0.5
 		echo "Iniciando descarga.."
-		youtube-dl -f bestaudio --extract-audio --audio-format mp3 --audio-quality 0 $Ind
-		sleep 0.5
+		youtube-dl -f bestaudio --embed-thumbnail --extract-audio --audio-format mp3 --audio-quality 0 $Ind
+		sleep 0.5 
 		echo $lineas
-		echo "Descarga finalizada, el archivo se encuentra en: "
+		echo "Descarga finalizada, el archivo se encuentra en: __>"
 		pwd
 		;;
 	[Pp]laylist|2)
-		read -p "Escribe la URL de la playlist " Play
+		read -p "Pega la URL de la playlist __>" Play
 		if [ -z $Play ];then
 			echo "No ingresante una url"
 			exit
 		fi
 		echo $lineas
 		sleep 0.5
-		echo "Preparando descarga.."
+		echo "Preparando descarga __>"
 		youtube-dl -ict --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 $Play
 		echo $lineas
 		;;
