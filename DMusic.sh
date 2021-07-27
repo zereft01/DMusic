90a7f6384337e96ba6a5ba3bf551982bbacb0741
zereft01 <80718011+zereft01@users.noreply.github.com>
2021-07-27 14:49:15 -0600

    Update DMusic.sh

--- DMusic.sh
+++ DMusic.sh
@@ -16,42 +16,42 @@ echo $lineas
 read -p "¿Que quieres descargar?
  [1] Canción 
  [2] Playlist 
- [3] Salir __>" arch
+ [3] Salir _>" arch
 
 
 case $arch in
 	[Cc]ancion|Canción|canción|1)
 		echo $lineas
-		read -p "Pega la URL de la canción __>" Ind
+		read -p "Pega la URL de la canción _>" Ind
 		if [ -z $Ind ];then
 			echo $lineas
-			echo "No ingresante una url"
+			echo "No ingresante una url _>"
 			exit
 		fi
 		
 		sleep 0.5
-		echo "Iniciando descarga.."
+		echo "Iniciando descarga _>"
 		youtube-dl -f bestaudio --embed-thumbnail --extract-audio --audio-format mp3 --audio-quality 0 $Ind
 		sleep 0.5 
 		echo $lineas
-		echo "Descarga finalizada, el archivo se encuentra en: __>"
+		echo "Descarga finalizada, el archivo se encuentra en: _>"
 		pwd
 		;;
 	[Pp]laylist|2)
-		read -p "Pega la URL de la playlist __>" Play
+		read -p "Pega la URL de la playlist _>" Play
 		if [ -z $Play ];then
-			echo "No ingresante una url"
+			echo "No ingresante una url _>"
 			exit
 		fi
 		echo $lineas
 		sleep 0.5
-		echo "Preparando descarga __>"
+		echo "Preparando descarga _>"
 		youtube-dl -ict --embed-thumbnail --yes-playlist --extract-audio --audio-format mp3 --audio-quality 0 $Play
 		echo $lineas
 		;;
 	 [3]|*)
 		echo $lineas
-		echo "Adiós"
+		echo "<_Adiós_>"
 		echo $lineas
 esac
 		
